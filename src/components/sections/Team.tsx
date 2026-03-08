40c330d266290ab2a6bfe995773fac4b93488df8
villagran-gif <villagran@clinyco.cl>
2026-03-08 11:33:40 -0300

    Create Team.tsx

---
+++ src/components/sections/Team.tsx
@@ -0,0 +1,37 @@
+const teamMembers = [
+  {
+    name: 'Dr. Nelson Aros',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Nelson Aros.webp'
+  },
+  {
+    name: 'Dr. Rodrigo Villagrán',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Rodrigo Villagran.webp'
+  },
+  {
+    name: 'Dr. Alberto Sirabo',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Alberto Sirabo.webp'
+  },
+  {
+    name: 'Dr. Ramón Díaz',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Ramon Diaz.webp'
+  },
+  {
+    name: 'Dr. Cristobal Guixé',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Cristobal Guixe.webp'
+  },
+  {
+    name: 'Dr. Ramón Díaz',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Ramon Diaz.webp'
+  },
+  {
+    name: 'Dr. Andrés San Martín',
+    role: 'Cirujano Bariátrico',
+    imageUrl: '../assets/Andres San Martin.webp'
+  }
+];
